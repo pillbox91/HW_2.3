4ccfedbe1902d0207d079e854696a888162c4d6f
Andrey Averyanov <averyanov1996@gmail.com>
2020-08-24 19:03:14 +0300

    Ready)

--- HW_2.3/ViewController.swift
+++ HW_2.3/ViewController.swift
@@ -10,11 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var viewColor: UIView!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var redSlider: UISlider!
+    
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var greenSlider: UISlider!
+    
+    @IBOutlet weak var blueLabel: UILabel!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        viewColor.layer.cornerRadius = 10
+        
+        let redSliderValue = CGFloat(redSlider.value)
+        let greenSliderValue = CGFloat(greenSlider.value)
+        let blueSliderValue = CGFloat(blueSlider.value)
+        
+        viewColor.backgroundColor = UIColor(red: redSliderValue, green: greenSliderValue, blue: blueSliderValue, alpha: 1)
+        
+       
+        
     }
+    
+    
 
-
+    @IBAction func redSliderAction() {
+        viewDidLoad()
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        
+    }
+    
+    @IBAction func greenSliderAction() {
+        viewDidLoad()
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+    }
+    
+    @IBAction func blueSliderAction() {
+        viewDidLoad()
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
+    }
 }
 
